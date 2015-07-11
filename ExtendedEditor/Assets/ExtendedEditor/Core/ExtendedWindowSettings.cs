6505a5840042b44984759175ce73ab726df28870
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-07-11 16:20:53 +0200

    Format

--- ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedWindowSettings.cs
+++ ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedWindowSettings.cs
@@ -7,15 +7,21 @@ namespace TNRD.Editor.Core {
 		/// Default is Assets/.../CurrentEditorFolder/Assets/
 		/// </summary>
 		public string AssetPath = "";
+
 		public bool AllowResize = true;
+
 		public bool AllowRepositioning = true;
+
+		public bool DrawTitleBarButtons = true;
 
 		public bool DrawToolbar = false;
 
 		public bool IsBlocking = true;
+
 		public bool IsFullscreen = true;
 
 		public bool UseCamera = false;
+
 		public bool UseOnSceneGUI = false;
 	}
 }
