6c0ff7dfa52e361ebe5741cd12419d03c45cee48
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-07-12 21:31:23 +0200

    Updated MultiWindowEditor

---
+++ ExtendedEditor/Assets/Examples/MultiWindowEditor/MultiWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using TNRD.Editor.Core;
+
+public class MultiWindow : ExtendedWindow {
+
+	public MultiWindow( string name, bool loose )
+		: base() {
+		Title = name;
+
+		Settings.IsBlocking = false;
+		Settings.IsFullscreen = false;
+
+		Settings.DrawTitleBarButtons = loose;
+		Settings.AllowRepositioning = Settings.AllowResize = loose;
+    }
+
+	public override void OnInitialize() {
+		base.OnInitialize();
+
+		if ( Settings.AllowRepositioning ) {
+			Size = new Vector2( 150, 150 );
+		} else {
+			Size = new Vector2( Editor.position.size.x / 2, Editor.position.size.y );
+		}
+	}
+}
--- ExtendedEditor/Assets/Examples/MultiWindowEditor/MultiWindowEditor.cs
+++ ExtendedEditor/Assets/Examples/MultiWindowEditor/MultiWindowEditor.cs
@@ -17,7 +17,15 @@ public class MultiWindowEditor : ExtendedEditor {
 
 		RepaintOnUpdate = true;
 
-		AddWindow( new MultiWindowOne() );
-		AddWindow( new MultiWindowTwo() );
+		var w1 = new MultiWindow( "#1", false );
+		var w2 = new MultiWindow( "#2", true );
+		var w3 = new MultiWindow( "#3", true );
+
+		AddWindow( w1 );
+		AddWindow( w2 );
+		AddWindow( w3 );
+
+		w2.Position = new Vector2( position.size.x / 2 + 50, 50 );
+		w3.Position = new Vector2( position.size.x / 2 + 100, 250 );
 	}
 }
