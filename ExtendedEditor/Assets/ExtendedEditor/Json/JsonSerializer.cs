02a00e37ea59ee200cb7acd6940a3e1183898c1b
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-02-18 13:13:15 +0100

    JsonType

--- ExtendedEditor/Assets/ExtendedEditor/Json/JsonSerializer.cs
+++ ExtendedEditor/Assets/ExtendedEditor/Json/JsonSerializer.cs
@@ -8,16 +8,6 @@ using System.Text.RegularExpressions;
 
 namespace TNRD.Editor.Json {
 
-    public struct JsonType : IEquatable<JsonType> {
-        public string Assembly;
-        public string Typename;
-
-        public bool Equals( JsonType other ) {
-            return other.Assembly == Assembly &&
-                other.Typename == Typename;
-        }
-    }
-
     public class JsonSerializer {
 
         public static string Serialize( object value ) {
