7c38f47a702ff44dbda68e4303b69d2c110c4e3b
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-03-02 22:22:44 +0100

    Clean

--- ExtendedEditor/Assets/ExtendedEditor/Json/JsonIgnoreAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace TNRD.Editor.Json {
-    [AttributeUsage( AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false )]
-    sealed class JsonIgnoreAttribute : Attribute {
-
-        public JsonIgnoreAttribute() { }
-    }
-}
