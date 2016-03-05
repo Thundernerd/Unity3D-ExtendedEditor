fc90f325136807c98209678c9ef7a8572c8d9763
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-03-05 13:36:19 +0100

    Some skeleton methods

--- ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedEditor.cs
+++ ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedEditor.cs
@@ -1,15 +1,168 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
 
-public class ExtendedEditor : MonoBehaviour {
+[Serializable]
+public class ExtendedEditor : EditorWindow {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [Serializable]
+    private class ReflectionData {
+        public MethodInfo Initialize;
+        public MethodInfo InitializeGUI;
+        public MethodInfo Destroy;
+
+        public MethodInfo Focus;
+        public MethodInfo LostFocus;
+
+        public MethodInfo Update;
+        public MethodInfo InspectorUpdate;
+
+        public MethodInfo GUI;
+
+        public ReflectionData() {
+            var type = typeof( ExtendedWindow );
+
+            Initialize = type.GetMethod( "InternalInitialize", BindingFlags.Instance | BindingFlags.NonPublic );
+            InitializeGUI = type.GetMethod( "InternalInitializeGUI", BindingFlags.Instance | BindingFlags.NonPublic );
+            Destroy = type.GetMethod( "InternalDestroy", BindingFlags.Instance | BindingFlags.NonPublic );
+
+            Focus = type.GetMethod( "InternalFocus", BindingFlags.Instance | BindingFlags.NonPublic );
+            LostFocus = type.GetMethod( "InternalLostFocus", BindingFlags.Instance | BindingFlags.NonPublic );
+
+            InspectorUpdate = type.GetMethod( "InternalInspectorUpdate", BindingFlags.Instance | BindingFlags.NonPublic );
+            Update = type.GetMethod( "InternalUpdate", BindingFlags.Instance | BindingFlags.NonPublic );
+
+            GUI = type.GetMethod( "InternalGUI", BindingFlags.Instance | BindingFlags.NonPublic );
+        }
+    }
+
+    public Vector2 Position {
+        get { return position.position; }
+        set {
+            var pos = position;
+            pos.position = value;
+            position = pos;
+        }
+    }
+    public Vector2 Size {
+        get { return position.size; }
+        set {
+            var pos = position;
+            pos.size = value;
+            position = pos;
+        }
+    }
+
+    private List<ExtendedWindow> windows = new List<ExtendedWindow>();
+
+    private bool isInitialized;
+    private bool isInitializedGUI;
+
+    private ReflectionData rData;
+
+    private void OnInitialize() {
+        rData = new ReflectionData();
+        isInitialized = true;
+    }
+
+    private void OnInitializeGUI() {
+        isInitializedGUI = true;
+    }
+
+    private void OnDestroy() {
+        for ( int i = windows.Count - 1; i >= 0; i-- ) {
+            rData.Destroy.Invoke( windows[i], null );
+            windows.RemoveAt( i );
+        }
+    }
+
+    private void OnFocus() {
+        for ( int i = windows.Count - 1; i >= 0; i-- ) {
+            rData.Focus.Invoke( windows[i], null );
+        }
+    }
+
+    private void OnLostFocus() {
+        for ( int i = windows.Count - 1; i >= 0; i-- ) {
+            rData.LostFocus.Invoke( windows[i], null );
+        }
+    }
+
+    private void OnGUI() {
+        if ( !isInitialized ) {
+            OnInitialize();
+            return;
+        }
+
+        if ( !isInitializedGUI ) {
+            OnInitializeGUI();
+            return;
+        }
+
+        var windowsToProcess = new List<ExtendedWindow>( windows );
+
+        BeginWindows();
+        for ( int i = windowsToProcess.Count - 1; i >= 0; i-- ) {
+            GUI.Window( i, windowsToProcess[i].WindowRect, WindowGUI, windowsToProcess[i].WindowContent );
+        }
+        EndWindows();
+    }
+
+    private void OnInspectorUpdate() {
+        var windowsToProcess = new List<ExtendedWindow>( windows );
+
+        for ( int i = 0; i < windowsToProcess.Count; i++ ) {
+            rData.InspectorUpdate.Invoke( windowsToProcess[i], null );
+        }
+    }
+
+    private void Update() {
+        var windowsToProcess = new List<ExtendedWindow>( windows );
+
+        for ( int i = 0; i < windowsToProcess.Count; i++ ) {
+            rData.Update.Invoke( windowsToProcess[i], null );
+        }
+    }
+
+    private void WindowGUI( int id ) {
+        if ( id < windows.Count ) {
+            rData.GUI.Invoke( windows[id], null );
+        }
+    }
+
+    public void AddWindow( ExtendedWindow window ) {
+        window.Editor = this;
+
+        rData.Initialize.Invoke( window, null );
+        windows.Add( window );
+    }
+
+    public void RemoveWindow( ExtendedWindow window ) {
+        rData.Destroy.Invoke( window, null );
+        windows.Remove( window );
+    }
+
+    public static ExtendedEditor CreateEditor( params ExtendedWindow[] windows ) {
+        var inst = GetWindow<ExtendedEditor>();
+
+        if ( windows.Length > 0 ) {
+            inst.rData = new ReflectionData();
+        }
+
+        foreach ( var item in windows ) {
+            inst.AddWindow( item );
+        }
+
+        return inst;
+    }
+
+    public static ExtendedEditor CreateEditor( string title, params ExtendedWindow[] windows ) {
+        var inst = CreateEditor( windows );
+
+        inst.titleContent = new GUIContent( title );
+
+        return inst;
+    }
 }
--- ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedWindow.cs
+++ ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedWindow.cs
@@ -1,15 +1,113 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
-public class ExtendedWindow : MonoBehaviour {
+[Serializable]
+public class ExtendedWindow {
+    
+    public GUIContent WindowContent = new GUIContent();
+    
+    public Rect WindowRect = new Rect();
+    
+    public Vector2 Position {
+        get { return WindowRect.position; }
+        set { WindowRect.position = value; }
+    }
+    
+    public Vector2 Size {
+        get { return WindowRect.size; }
+        set { WindowRect.size = value; }
+    }
+    
+    public ExtendedEditor Editor;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private bool initializedGUI = false;
+
+    private void InternalInitialize() {
+        if ( Position == Vector2.zero && Size == Vector2.zero ) {
+            WindowRect = new Rect( Vector2.zero, Editor.Size );
+        }
+
+        OnInitialize();
+    }
+
+    private void InternalInitializeGUI() {
+        OnInitializeGUI();
+        initializedGUI = true;
+    }
+
+    private void InternalDestroy() {
+        OnDestroy();
+    }
+
+    private void InternalFocus() {
+        OnFocus();
+    }
+
+    private void InternalLostFocus() {
+        OnLostFocus();
+    }
+
+    private void InternalInspectorUpdate() {
+        OnInspectorUpdate();
+    }
+
+    private void InternalUpdate() {
+        OnUpdate();
+    }
+
+    private void InternalGUI() {
+        if ( !initializedGUI ) {
+            InternalInitializeGUI();
+        }
+
+        OnGUI();
+    }
+
+    protected virtual void OnInitialize() {
+
+    }
+
+    protected virtual void OnInitializeGUI() {
+
+    }
+
+    protected virtual void OnDestroy() {
+
+    }
+
+    protected virtual void OnFocus() {
+
+    }
+
+    protected virtual void OnLostFocus() {
+
+    }
+
+    protected virtual void OnGUI() {
+
+    }
+
+    protected virtual void OnInspectorUpdate() {
+
+    }
+
+    protected virtual void OnUpdate() {
+
+    }
+
+    public static void CreateEditor() {
+        CreateEditor( "" );
+    }
+
+    public static void CreateEditor( string title ) {
+        var stack = new System.Diagnostics.StackTrace();
+        if ( stack.FrameCount >= 1 ) {
+            var mBase = stack.GetFrame( 1 ).GetMethod();
+            var type = mBase.DeclaringType;
+            var instance = (ExtendedWindow)System.Activator.CreateInstance( type );
+            ExtendedEditor.CreateEditor( title, instance );
+        } else {
+            Debug.LogError( "Error creating editor" );
+        }
+    }
 }
