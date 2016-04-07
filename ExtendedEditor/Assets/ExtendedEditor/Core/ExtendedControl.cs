2834ebdb999a4d443b916b21c95b03ec70456fd2
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-04-07 09:35:22 +0200

    Added anchor point for controls

--- ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedControl.cs
+++ ExtendedEditor/Assets/ExtendedEditor/Core/ExtendedControl.cs
@@ -12,7 +12,30 @@ namespace TNRD.Editor.Core {
         public ExtendedWindow Window;
 
         public Rect Rectangle {
-            get { return new Rect( Position, Size ); }
+            get {
+                switch ( AnchorPoint ) {
+                    case EAnchor.TopLeft:
+                        return new Rect( Position, Size );
+                    case EAnchor.TopCenter:
+                        return new Rect( Position - new Vector2( Size.x / 2, 0 ), Size );
+                    case EAnchor.TopRight:
+                        return new Rect( Position - new Vector2( Size.x, 0 ), Size );
+                    case EAnchor.MiddleLeft:
+                        return new Rect( Position - new Vector2( 0, Size.y / 2 ), Size );
+                    case EAnchor.MiddleCenter:
+                        return new Rect( Position - ( Size / 2 ), Size );
+                    case EAnchor.MiddleRight:
+                        return new Rect( Position - new Vector2( Size.x, Size.y / 2 ), Size );
+                    case EAnchor.BottomLeft:
+                        return new Rect( Position - new Vector2( 0, Size.y ), Size );
+                    case EAnchor.BottomCenter:
+                        return new Rect( Position - new Vector2( Size.x / 2, Size.y ), Size );
+                    case EAnchor.BottomRight:
+                        return new Rect( Position - Size, Size );
+                    default:
+                        return new Rect( Position, Size );
+                }
+            }
         }
 
         public Vector2 Position;
@@ -26,6 +49,8 @@ namespace TNRD.Editor.Core {
         public ExtendedInput Input {
             get { return Window.Input; }
         }
+
+        public EAnchor AnchorPoint = EAnchor.MiddleCenter;
 
         [JsonProperty]
         private bool initializedGUI;
---
+++ ExtendedEditor/Assets/ExtendedEditor/EAnchor.cs
@@ -0,0 +1,14 @@
+﻿namespace TNRD.Editor {
+
+    public enum EAnchor {
+        TopLeft = 0,
+        TopCenter,
+        TopRight,
+        MiddleLeft,
+        MiddleCenter,
+        MiddleRight,
+        BottomLeft,
+        BottomCenter,
+        BottomRight
+    }
+}
